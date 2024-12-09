084f49a78160b1b5df05a8e55fcc2569ce0fc305
Tanaka878 <musungaretanaka@gmail.com>
2024-12-09 12:17:57 +0200

    styling and cerating the login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Login from "./components/Login";
-import SignUp from "./components/SignUp";
+import Login from "./components/Login/Login";
+
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Login/>
+
+
       
     </div>
   );
