8b7ae89ca3198c9e2d94296d447262b4c513687b
Tanaka878 <musungaretanaka@gmail.com>
2024-12-10 12:11:28 +0200

    complete profile with static data

--- src/app/HomePage.tsx
+++ src/app/HomePage.tsx
@@ -2,17 +2,18 @@
 
 import React from 'react'
 import Picture from './components/ProfileData/Picture'
+import PersonalData from './components/ProfileData/PersonalData'
 
 const HomePage = () => {
   return (
     <div>
-        <div className='flex'>
-            {/* For containing the top corner*/ }
-            <Picture/>
-
+      <div className="fixed top-0 left-0 right-0 z-10 flex justify-center p-4 bg-white shadow-md">
+        {/* For containing the top corner */}
+        <div className="flex items-center space-x-6">
+          <Picture />
+          <PersonalData />
         </div>
-        
-      
+      </div>
     </div>
   )
 }
