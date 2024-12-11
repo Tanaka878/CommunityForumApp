4cdaa3fd06d7351d9404baa645f2a0f8bd5456c9
Tanaka878 <musungaretanaka@gmail.com>
2024-12-11 15:51:51 +0200

    community card

--- src/app/HomePage.tsx
+++ src/app/HomePage.tsx
@@ -3,8 +3,15 @@ import React from 'react'
 import ProfileImage from './components/GroupsContainer/ProfileImage'
 import PersonalData from './components/ProfileData/PersonalData'
 import GroupCommunityCard from './components/GroupCard/CommunityCard'
+import SearchBar from './components/SearchBar/SearchBar'
 
 const HomePage = () => {
+
+  
+    function handleSearch(): void {
+      throw new Error('Function not implemented.')
+    }
+
   return (
     <div>
 
@@ -13,10 +20,19 @@ const HomePage = () => {
         <PersonalData/>
       </nav>
 
+      <nav className='mt-2'>
+        <h1 className='border-l-fuchsia-500 mt-2 font-extrabold justify-center mx-auto'>Communites</h1>
+      </nav>
+
+      <SearchBar onSearch={handleSearch}/>
+
       <div className='grid mt-3 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4'>
         <GroupCommunityCard/>
         <GroupCommunityCard/>
         <GroupCommunityCard/>
+        <GroupCommunityCard/>
+        <GroupCommunityCard/>
+
       </div>
       
     </div>
