2688d22dbdf5efb52a698f9a5ac97929412a7ca1
Tanaka878 <musungaretanaka@gmail.com>
2025-01-13 13:18:18 +0200

    commit

--- src/app/components/Settings/HomePage/page.tsx
+++ src/app/components/Settings/HomePage/page.tsx
@@ -3,10 +3,11 @@ import { ArrowRight } from 'lucide-react'
 
 const Settings = () => {
   return (
-    <div>
+    <div className='p-7 items-center'>
+      <h1 className='items-center font-bold text-5xl '>Settings</h1>
       <nav className='justify-between flex'>
-        <h2>Profile</h2>
-        <ArrowRight/>
+        <h2 className='font-extrabold text-2xl'>Profile</h2>
+        <ArrowRight className='font-extrabold text-xl'/>
       </nav>
       
         
