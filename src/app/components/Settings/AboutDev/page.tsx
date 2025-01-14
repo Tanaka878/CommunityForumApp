b7d21a0e58301627629bb918b1c61c38c5d5c4da
Tanaka878 <musungaretanaka@gmail.com>
2025-01-14 14:24:44 +0200

    commit

--- src/app/components/Settings/AboutDev/page.tsx
+++ src/app/components/Settings/AboutDev/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Image from 'next/image';
 import React from 'react'
 import { MdEmail } from 'react-icons/md';
 
@@ -8,7 +9,16 @@ const page = () => {
         window.location.href ="mailto:musungaretanaka@gmail.com"
     }
   return (
-    <div>
+    <div className='bg-slate-600'>
+        <Image
+        src={'/Images/header.png'}
+        height={150}
+        width={120}
+        alt='Dev Image'
+        className='rounded-full text-center justify-center'
+        />
+        <h1 className='text-5xl p-4 text-center first-letter:text-rose-700 '>Hi MATE .</h1>
+        <p className='text-pretty tracking-tight'> My name is Tanaka Musungare an INFORMATION SYSTEMS Student at Midlands State University</p>
         <nav  className='flex'>
             <button type="button" onClick={handleMail} className=''>
                 <MdEmail className='h-14 w-11'/>
