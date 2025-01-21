a88e7d7aaf091d4c4653b522164bf006ff12cb6e
Tanaka878 <musungaretanaka@gmail.com>
2025-01-21 10:49:04 +0200

    adding and configuring mongo serveless instance

--- src/app/components/SignUp/page.tsx
+++ src/app/components/SignUp/page.tsx
@@ -51,7 +51,7 @@ const SignUp = () => {
     const credentials = { email, password, nickname };
 
     try {
-      const response = await fetch('http://localhost:8080/api/v1/auth/register/', {
+      const response = await fetch(`${BASE_URL}/api/v1/auth/register/`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- src/app/config/api.ts
+++ src/app/config/api.ts
@@ -1,3 +1,3 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://communityforumbackend-production.up.railway.app";
 
 export default BASE_URL;
